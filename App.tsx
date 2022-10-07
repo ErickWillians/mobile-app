91a352fb783404f3e6b51b8a697748a3b07dcfa1
Erick Willians <erickw.gw@gmail.com>
2022-10-07 16:20:37 -0300

    mobile-app-under-construction

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/Home'; 
+import { Background } from './src/componentes/Background';
+import { Loading } from './src/componentes/Loading';
+
+export default function App() {
+ const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+    <Background>
+      <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent      
+      />
+
+      { fontsLoaded ? <Home/> : <Loading/>}
+
+    </Background>
+  );
+}
